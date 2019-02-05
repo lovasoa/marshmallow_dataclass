113efe3f7c9d351d87d75bd3bdaac803ff4320b4
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2019-02-05 17:00:41 +0100

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.0'
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.7',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries'
+]
+
+setup(
+    name='marshmallow_dataclass',
+    version=VERSION,
+    description='Python library to convert dataclasses into marshmallow schemas.',
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    author='Ophir LOJKINE',
+    author_email='pere.jobs@gmail.com',
+    url='https://github.com/lovasoa/marshmallow_dataclass',
+    keywords=['marshmallow', 'dataclass', 'serialization'],
+    classifiers=CLASSIFIERS,
+    install_requires=['marshmallow']
+)
