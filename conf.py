db01aef93298860242494b0cafb655bca67abac1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-20 15:16:36 +0000

    Deploy lovasoa/marshmallow_dataclass to github.com/lovasoa/marshmallow_dataclass.git:gh-pages

--- conf.py
+++ conf.py
@@ -114,37 +114,6 @@ html_static_path = ["_static"]
 htmlhelp_basename = "marshmallow_dataclassdoc"
 
 
-# -- Options for LaTeX output ------------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (
-        master_doc,
-        "marshmallow_dataclass.tex",
-        "marshmallow\\_dataclass Documentation",
-        "Ophir LOJKINE",
-        "manual",
-    )
-]
-
-
 # -- Options for manual page output ------------------------------------------
 
 # One entry per manual page. List of tuples
