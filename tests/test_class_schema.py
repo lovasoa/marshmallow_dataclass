ae0de58957649ed0edf979f8e66d11bfee946f3e
Zoran Pavlovic <zoran@bbd.co.za>
2020-02-24 13:18:29 +0200

    Fixed formatting using black formatter.

--- tests/test_class_schema.py
+++ tests/test_class_schema.py
@@ -5,7 +5,6 @@ from marshmallow_dataclass import class_schema
 
 
 class TestClassSchema(unittest.TestCase):
-
     def test_simple_unique_schemas(self):
         @dataclasses.dataclass
         class Simple:
@@ -17,15 +16,27 @@ class TestClassSchema(unittest.TestCase):
             three: int = dataclasses.field()
             four: Simple = dataclasses.field()
 
-        self.assertEqual(id(class_schema(ComplexNested)), id(class_schema(ComplexNested)))
+        self.assertEqual(
+            id(class_schema(ComplexNested)), id(class_schema(ComplexNested))
+        )
         self.assertTrue(class_schema(ComplexNested) is class_schema(ComplexNested))
         self.assertEqual(id(class_schema(Simple)), id(class_schema(Simple)))
         self.assertTrue(class_schema(Simple) is class_schema(Simple))
-        self.assertTrue(class_schema(Simple) is class_schema(ComplexNested)._declared_fields["four"].nested)
+        self.assertTrue(
+            class_schema(Simple)
+            is class_schema(ComplexNested)._declared_fields["four"].nested
+        )
 
-        complex_set = {class_schema(ComplexNested), class_schema(ComplexNested, base_schema=None),
-                       class_schema(ComplexNested, None)}
-        simple_set = {class_schema(Simple), class_schema(Simple, base_schema=None), class_schema(Simple, None)}
+        complex_set = {
+            class_schema(ComplexNested),
+            class_schema(ComplexNested, base_schema=None),
+            class_schema(ComplexNested, None),
+        }
+        simple_set = {
+            class_schema(Simple),
+            class_schema(Simple, base_schema=None),
+            class_schema(Simple, None),
+        }
         self.assertEqual(len(complex_set), 1)
         self.assertEqual(len(simple_set), 1)
 
