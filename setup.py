c7d684b473ad7bdc564bff367ca26f3e543a30f3
Jeff Dairiki <dairiki@dairiki.org>
2022-11-05 09:24:03 -0700

    Test under Python 3.11, update CI workflow to latest versions of actions (#220)
    * Test under Python 3.11
    
    * Do not set PYTHONWARNINGS=error during CI pytest
    
    That catches, e.g., deprecation warnings from any package (beyond our
    control).
    
    Note that we do configure a pytest warning filter in pyproject.toml
    which will generate errors for any warnings generated from within our
    code.
    
    * CI: update to actions/checkout@v3, actions/setup-python@v4, actions/cache@v3
    
    * Hack installation of PyYAML on pypy
    
    Currently PyYAML fails to build with Cython 3.0.0a, thus
    it fails to install from sdist using `pip install --pre`.
    
    See https://github.com/yaml/pyyaml/issues/601
    
    * Update pre-commit plugin versions to latest

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: 3.11",
     "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
 ]
 
