d41be98e66c420783d7d7f4f849eabd95b1aa076
Jeff Dairiki <dairiki@dairiki.org>
2023-09-15 23:18:48 -0700

    Revert "skip failing test on python3.6"
    This reverts commit e8e29b9a45ad76d7894910b9350cb025071c28b8.

--- tests/test_field_for_schema.py
+++ tests/test_field_for_schema.py
@@ -156,10 +156,6 @@ class TestFieldForSchema(unittest.TestCase):
             field_for_schema(Final), fields.Raw(required=True, allow_none=True)
         )
 
-    @unittest.skipIf(
-        sys.version_info < (3, 7),
-        "union ordering is not guaranteed in python < 3.7",
-    )
     def test_union(self):
         self.assertFieldsEqual(
             field_for_schema(Union[int, str]),
