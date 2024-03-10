48b7e1768e6db9f8397a204c3ed0ab87225ec23c
Michiel <918128+mvanderlee@users.noreply.github.com>
2024-03-10 02:01:06 +0100

    Add support back for Python3.8

--- marshmallow_dataclass/typing.py
+++ marshmallow_dataclass/typing.py
@@ -1,6 +1,11 @@
-from typing import Annotated
+import sys
 
 import marshmallow.fields
+
+if sys.version_info >= (3, 9):
+    from typing import Annotated
+else:
+    from typing_extensions import Annotated
 
 Url = Annotated[str, marshmallow.fields.Url]
 Email = Annotated[str, marshmallow.fields.Email]
