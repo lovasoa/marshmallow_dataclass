91f3977cb0a0e509325938127f66107c7113e3b4
Colin Watson <cjwatson@debian.org>
2026-03-05 13:17:03 +0000

    Test on Python 3.13 and 3.14 (#287)
    * Test on Python 3.13 and 3.14
    
    * pre-commit: Upgrade flake8, flake8-bugbear, and pyupgrade
    
    * Only run pre-commit on one Python version

--- setup.py
+++ setup.py
@@ -12,6 +12,8 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
+    "Programming Language :: Python :: 3.14",
     "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
 ]
 
